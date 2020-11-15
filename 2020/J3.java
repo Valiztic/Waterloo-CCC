c880310439a9e80fd8d14500ad6f365864bb425d
Valiztic <68851311+Valiztic@users.noreply.github.com>
2020-11-14 21:09:19 -0800

    Create J3.java

---
+++ 2020/J3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class J3
+{
+	public static void main(String[]args)
+	{
+		Scanner s = new Scanner(System.in);
+		int p = s.nextInt();
+		int n = s.nextInt();
+		int r = s.nextInt();
+		int a = n;
+		int count = 0;
+
+		while(a <= p)
+		{
+			n *= r;
+			a += n;
+			System.out.println(a);
+			count++;
+		}
+		System.out.print(count);
+	}
+}
