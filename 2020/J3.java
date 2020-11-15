ed1676da18a2694de913bc1c93b9016b4010ab41
Valiztic <68851311+Valiztic@users.noreply.github.com>
2020-11-14 21:19:34 -0800

    Accidentally copied J2 twice (fixed)

--- 2020/J3.java
+++ 2020/J3.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class J3
 {
 	public static void main(String[]args)
 	{
 		Scanner s = new Scanner(System.in);
-		int p = s.nextInt();
-		int n = s.nextInt();
-		int r = s.nextInt();
-		int a = n;
-		int count = 0;
-
-		while(a <= p)
+		int limit = s.nextInt();
+		int[] a = new int[limit];
+		int[] b = new int[limit];
+		for (int i = 0; i < limit; i++)
 		{
-			n *= r;
-			a += n;
-			System.out.println(a);
-			count++;
+			String[] c = s.next().split(",");
+			a[i] = Integer.parseInt(c[0]);
+			b[i] = Integer.parseInt(c[1]);
 		}
-		System.out.print(count);
+		Arrays.sort(a);
+		Arrays.sort(b);
+		System.out.print((a[0]-1) + "," + (b[0]-1) + "\n" + (a[limit-1]+1) + "," + (b[limit-1]+1));
 	}
 }
